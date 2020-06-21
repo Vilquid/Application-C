55ae699ef48d97b31b5532d278b12a1051a49733
samudec <51175799+samudec@users.noreply.github.com>
2020-06-21 16:43:58 +0200

    Add files via upload

---
+++ interface/header.h
@@ -0,0 +1,9 @@
+#include <stdlib.h> // Pour pouvoir utiliser exit()
+#include <stdio.h> // Pour pouvoir utiliser printf()
+#include <math.h> // Pour pouvoir utiliser sin() et cos()
+#include "../Lib/GfxLib.h" // Seul cet include est necessaire pour faire du graphique
+#include "../Lib/BmpLib.h" // Cet include permet de manipuler des fichiers BMP
+#include "../Lib/ESLib.h" // Pour utiliser valeurAleatoire()
+  
+
+
